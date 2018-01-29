69dbdb11145d4bace95021e0c52d90e3fbc0d000
WangQilin <qilin.wang@huawei.com>
2018-01-29 17:08:57 +0800

    fix go get errors
    Signed-off-by: WangQilin <qilin.wang@huawei.com>

--- core/tracing/collector_test.go
+++ core/tracing/collector_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/ServiceComb/go-chassis/core/tracing"
 	"github.com/apache/thrift/lib/go/thrift"
 	zipkin "github.com/openzipkin/zipkin-go-opentracing"
-	"github.com/openzipkin/zipkin-go-opentracing/_thrift/gen-go/zipkincore"
+	"github.com/openzipkin/zipkin-go-opentracing/thrift/gen-go/zipkincore"
 	"github.com/stretchr/testify/assert"
 )
 
