172a1c50fbc703c8d5f0fad4f87faf3646ef1d32
asifdxtreme <mohammad.asif.siddiqui1@huawei.com>
2018-02-05 16:13:13 +0530

    Update the location of cloning SC

--- scripts/travis/start_latest_sc.sh
+++ scripts/travis/start_latest_sc.sh
@@ -9,4 +9,12 @@ bash -x scripts/release/make_release.sh linux latest latest
 cd servicecomb-service-center-latest-linux-amd64
 bash -x start.sh
 
+sleep 5
 
+echo "Testing if Service-Center started succesfully"
+
+curl -v http://127.0.0.1:30100/version
+
+curl -v http://127.0.0.1:30100/v4/default/registry/version
+
+curl -v http://127.0.0.1:30100/health
