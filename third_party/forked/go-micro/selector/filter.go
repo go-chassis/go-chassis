eeb927b576254bf08a0901a50f845f8f059515c2
Shawn <xiaoliang.tian@gmail.com>
2018-02-09 15:12:33 +0800

    delete unused func (#79)

--- third_party/forked/go-micro/selector/filter.go
+++ third_party/forked/go-micro/selector/filter.go
@@ -6,11 +6,6 @@ import (
 	"github.com/ServiceComb/go-chassis/core/registry"
 )
 
-// constant string for zoneaware
-const (
-	ZoneAware = "zoneaware"
-)
-
 // FilterEndpoint is an endpoint based Select Filter which will
 // only return services with the endpoint specified.
 func FilterEndpoint(target string) Filter {
@@ -40,26 +35,6 @@ func FilterMD(key, val string) Filter {
 			if ins.Metadata[key] == val {
 				instances = append(instances, ins)
 			}
-		}
-
-		return instances
-	}
-}
-
-// FilterProtocol is for filtering the instances based on protocol
-func FilterProtocol(protocol string) Filter {
-	return func(old []*registry.MicroServiceInstance) []*registry.MicroServiceInstance {
-		var instances []*registry.MicroServiceInstance
-
-		for _, ins := range old {
-			for p := range ins.EndpointsMap {
-				if p == protocol {
-					instances = append(instances, ins)
-					break
-				}
-
-			}
-
 		}
 
 		return instances
