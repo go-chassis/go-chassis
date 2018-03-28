6dcbb383ddb2bd846532283cee3958e08db05709
little-cui <sure_0757@qq.com>
2018-03-28 21:55:56 +0800

    New invoker

--- core/rpc_invoker.go
+++ core/rpc_invoker.go
@@ -5,10 +5,7 @@ import (
 	"sync"
 
 	"github.com/ServiceComb/go-chassis/core/common"
-	"github.com/ServiceComb/go-chassis/core/config"
-	"github.com/ServiceComb/go-chassis/core/handler"
 	"github.com/ServiceComb/go-chassis/core/invocation"
-	"github.com/ServiceComb/go-chassis/core/lager"
 )
 
 // RPCInvoker is rpc invoker
@@ -33,19 +30,6 @@ func NewRPCInvoker(opt ...Option) *RPCInvoker {
 	return ri
 }
 
-// newOptions is for updating options
-func newOptions(options ...Option) Options {
-	opts := DefaultOptions
-
-	for _, o := range options {
-		o(&opts)
-	}
-	if opts.ChainName == "" {
-		opts.ChainName = common.DefaultChainName
-	}
-	return opts
-}
-
 // Invoke is for to invoke the functions during API calls
 func (ri *RPCInvoker) Invoke(ctx context.Context, microServiceName, schemaID, operationID string, arg interface{}, reply interface{}, options ...InvocationOption) error {
 	opts := getOpts(microServiceName, options...)
@@ -62,31 +46,4 @@ func (ri *RPCInvoker) Invoke(ctx context.Context, microServiceName, schemaID, op
 	i.Reply = reply
 	i.Ctx = ctx
 	return ri.invoke(i, reply)
-}
-
-// abstract invoker is a common invoker for RPC
-type abstractInvoker struct {
-	opts Options
-}
-
-func (ri *abstractInvoker) invoke(i *invocation.Invocation, reply interface{}) error {
-	if len(i.Filters) == 0 {
-		i.Filters = ri.opts.Filters
-	}
-
-	// add self service name into remote context, this value used in provider rate limiter
-	i.Ctx = common.WithContext(i.Ctx, common.HeaderSourceName, config.SelfServiceName)
-
-	c, err := handler.GetChain(common.Consumer, ri.opts.ChainName)
-	if err != nil {
-		lager.Logger.Errorf(err, "Handler chain init err.")
-		return err
-	}
-
-	c.Next(i, func(ir *invocation.InvocationResponse) error {
-		err = ir.Err
-		reply = ir.Result
-		return err
-	})
-	return err
 }
