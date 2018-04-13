bf4b9b6993ad5b23d12a7b911bb19e0e4556774c
Shawn <xiaoliang.tian@gmail.com>
2018-04-13 16:59:51 +0800

    init read the doc (#175)

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,6 @@ from recommonmark.parser import CommonMarkParser
 source_parsers = {
     '.md': CommonMarkParser,
 }
-
 source_suffix = ['.rst', '.md']
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -52,8 +51,7 @@ templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
-#
-# source_suffix = ['.rst', '.md']
+source_suffix = ['.rst', '.md']
 
 # The master toctree document.
 master_doc = 'index'
