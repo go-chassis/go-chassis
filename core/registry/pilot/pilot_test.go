7c9944c078d0f4618d47ccd65da168badf3b9345
little-cui <sure_0757@qq.com>
2018-04-17 22:22:31 +0800

    Fix the UT failure

--- core/registry/pilot/pilot_test.go
+++ core/registry/pilot/pilot_test.go
@@ -10,7 +10,6 @@ import (
 
 var (
 	s  = httptest.NewServer(&mockPilotHandler{})
-	r  = newRegistrator(registry.Options{Addrs: []string{s.Listener.Addr().String()}})
 	sd = newDiscoveryService(registry.Options{Addrs: []string{s.Listener.Addr().String()}})
 )
 
@@ -20,22 +19,11 @@ func init() {
 }
 
 func TestPilot_RegisterServiceAndInstance(t *testing.T) {
-	microservice := &registry.MicroService{
-		ServiceName: "a",
-	}
-	microServiceInstance := &registry.MicroServiceInstance{
-		EndpointsMap: map[string]string{"rest": "1.1.1.1:80"},
-	}
-	serviceID, instanceID, err := r.RegisterServiceAndInstance(microservice, microServiceInstance)
-	assert.NoError(t, err)
-	assert.Equal(t, "a", serviceID)
-	assert.Equal(t, "1.1.1.1_80", instanceID)
-
-	microservice, err = sd.GetMicroService("a")
+	microservice, err := sd.GetMicroService("a")
 	assert.NoError(t, err)
 	assert.Equal(t, "a", microservice.ServiceName)
 
-	serviceID, err = sd.GetMicroServiceID("", "a", "", "")
+	serviceID, err := sd.GetMicroServiceID("", "a", "", "")
 	assert.NoError(t, err)
 	assert.Equal(t, "a", serviceID)
 
@@ -46,12 +34,14 @@ func TestPilot_RegisterServiceAndInstance(t *testing.T) {
 	instances, err := sd.GetMicroServiceInstances("", "a")
 	assert.NoError(t, err)
 	assert.NotEqual(t, 0, len(instances))
-	assert.Equal(t, instanceID, instances[0].InstanceID)
-	assert.Equal(t, microServiceInstance.EndpointsMap["rest"], instances[0].EndpointsMap["rest"])
+	assert.Equal(t, "1.1.1.1_80", instances[0].InstanceID)
+	assert.Equal(t, "1.1.1.1:80", instances[0].EndpointsMap["rest"])
 
 	instances, err = sd.FindMicroServiceInstances("", "", "a", "", "")
 	assert.NoError(t, err)
 	assert.NotEqual(t, 0, len(instances))
-	assert.Equal(t, instanceID, instances[0].InstanceID)
-	assert.Equal(t, microServiceInstance.EndpointsMap["rest"], instances[0].EndpointsMap["rest"])
+	assert.Equal(t, "1.1.1.1_80", instances[0].InstanceID)
+	assert.Equal(t, "1.1.1.1:80", instances[0].EndpointsMap["rest"])
+
+	sd.Close()
 }
