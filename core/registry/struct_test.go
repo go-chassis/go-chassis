5069b93489e0ffad5165e9def6506ef2fc28e721
tian <xiaoliang.tian@gmail.com>
2018-12-10 16:56:36 +0800

    fix bug: can not save service with different app value

---
+++ core/registry/struct_test.go
@@ -0,0 +1,48 @@
+package registry
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestMicroServiceInstance_Equal(t *testing.T) {
+	ins1 := &MicroServiceInstance{
+		InstanceID: "1",
+		ServiceID:  "bill",
+	}
+	ins2 := &MicroServiceInstance{
+		InstanceID: "1",
+		ServiceID:  "bill",
+	}
+	assert.True(t, ins1.Equal(ins2))
+
+	ins3 := &MicroServiceInstance{
+		InstanceID: "1",
+		ServiceID:  "bill",
+		Metadata: map[string]string{
+			"a": "b",
+			"c": "d",
+		},
+	}
+	ins4 := &MicroServiceInstance{
+		InstanceID: "1",
+		ServiceID:  "bill",
+		Metadata: map[string]string{
+			"a": "b",
+			"c": "d",
+		},
+	}
+	assert.True(t, ins3.Equal(ins4))
+
+	ins5 := &MicroServiceInstance{
+		InstanceID: "2",
+		ServiceID:  "bill",
+	}
+	assert.False(t, ins5.Equal(ins4))
+
+	ins6 := &MicroServiceInstance{
+		InstanceID: "2",
+		ServiceID:  "text",
+	}
+	assert.False(t, ins5.Equal(ins6))
+}
