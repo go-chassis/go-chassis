1650ce54b1529bff2e76952780e51b2f6f38a20b
thandayuthapani <thandayuthapani.s@huawei.com>
2018-12-27 18:52:44 +0530

    Add testcase for fault injection

---
+++ core/fault/fault_injection_test.go
@@ -0,0 +1,25 @@
+package fault_test
+
+import (
+	"fmt"
+	"github.com/go-chassis/go-chassis/core/config/model"
+	"github.com/go-chassis/go-chassis/core/fault"
+	"github.com/go-chassis/go-chassis/core/invocation"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func Test_ApplyFaultInjection(t *testing.T) {
+	var inv = new(invocation.Invocation)
+	var fault1 = new(model.Fault)
+	inv.Endpoint = "1.2.3.4"
+	inv.MicroServiceName = "Server"
+	fault1.Delay.FixedDelay = 2 * time.Second
+	//delay must not return error
+	v := fault.ApplyFaultInjection(fault1, inv, 100, "delay")
+	assert.Equal(t, v, nil)
+	//abort must return error
+	v = fault.ApplyFaultInjection(fault1, inv, 100, "abort")
+	assert.Equal(t, v, fmt.Errorf("injecting abort"))
+}
