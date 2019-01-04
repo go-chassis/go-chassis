882c390011e34173ffc5c7a44fa1fa42cc97fef4
Shawn <xiaoliang.tian@gmail.com>
2019-01-04 14:59:13 +0800

    grpc protocol support provider chain (#494)

--- server/grpc/server.go
+++ server/grpc/server.go
@@ -3,9 +3,16 @@ package grpc
 import (
 	"crypto/tls"
 	"errors"
+	"fmt"
+	"github.com/go-chassis/go-chassis/core/common"
+	"github.com/go-chassis/go-chassis/core/handler"
+	"github.com/go-chassis/go-chassis/core/invocation"
 	"github.com/go-chassis/go-chassis/core/server"
+	"github.com/go-chassis/go-chassis/pkg/runtime"
 	"github.com/go-mesh/openlogging"
+	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 	"net"
 )
 
@@ -25,11 +32,51 @@ type Server struct {
 	opts server.Options
 }
 
+//Request2Invocation convert grpc protocol to invocation
+func Request2Invocation(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo) *invocation.Invocation {
+	md, _ := metadata.FromIncomingContext(ctx)
+	sourceServices := md.Get(common.HeaderSourceName)
+	var sourceService string
+	if len(sourceServices) >= 1 {
+		sourceService = sourceServices[0]
+	}
+	//TODO maybe need set headers
+	m := make(map[string]string, 0)
+	inv := &invocation.Invocation{
+		MicroServiceName:   runtime.ServiceName,
+		SourceMicroService: sourceService,
+		Args:               req,
+		Protocol:           "grpc",
+		SchemaID:           info.FullMethod,
+		OperationID:        info.FullMethod,
+		Ctx:                context.WithValue(ctx, common.ContextHeaderKey{}, m),
+	}
+	return inv
+}
+
 //New create grpc server
 func New(opts server.Options) server.ProtocolServer {
+	interceptor := func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handle grpc.UnaryHandler) (resp interface{}, err error) {
+		c, err := handler.GetChain(common.Provider, opts.ChainName)
+		if err != nil {
+			openlogging.Error(fmt.Sprintf("Handler chain init err [%s]", err.Error()))
+			return nil, err
+		}
+		inv := Request2Invocation(ctx, req, info)
+		var r *invocation.Response
+		c.Next(inv, func(ir *invocation.Response) error {
+			if ir.Err != nil {
+				return ir.Err
+			}
+			ir.Result, ir.Err = handle(ctx, req)
+			r = ir
+			return ir.Err
+		})
+		return r.Result, r.Err
+	}
 	return &Server{
 		opts: opts,
-		s:    grpc.NewServer(),
+		s:    grpc.NewServer(grpc.UnaryInterceptor(interceptor)),
 	}
 }
 
