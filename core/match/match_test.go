0746650d9cc5fd51a63a1eb07ce74df9487ba630
fanhui <fanh1984@gmail.com>
2020-04-09 20:29:56 +0800

    add unit test

--- core/match/match_test.go
+++ core/match/match_test.go
@@ -18,8 +18,12 @@
 package match_test
 
 import (
+	"context"
+	"github.com/go-chassis/go-chassis/client/rest"
+	"github.com/go-chassis/go-chassis/core/invocation"
 	"github.com/go-chassis/go-chassis/core/match"
 	"github.com/stretchr/testify/assert"
+	"net/http"
 	"testing"
 )
 
@@ -35,7 +39,7 @@ func TestMatch(t *testing.T) {
 	assert.False(t, b)
 }
 
-func TestMark(t *testing.T) {
+func TestSaveMatchPolicy(t *testing.T) {
 	testName := "match-user-json"
 	testMatchPolic := `
         headers:
@@ -48,5 +52,51 @@ func TestMark(t *testing.T) {
         method: GET
 	`
 	match.SaveMatchPolicy(testMatchPolic, "servicecomb.match."+testName, testName)
+}
 
+func TestMark(t *testing.T) {
+
+	t.Run("test match header", func(t *testing.T) {
+		testName := "match-user-json-header"
+		testMatchPolic := `
+        headers:
+          user:
+            exact: jason`
+		match.SaveMatchPolicy(testMatchPolic, "servicecomb.match."+testName, testName)
+		i := createInvoker(map[string]string{
+			"user": "jason",
+		}, http.MethodPost, "")
+		match.Mark(i)
+		assert.Equal(t, testName, i.GetMark())
+	})
+	t.Run("test match method", func(t *testing.T) {
+		testName := "match-user-json-method"
+		testMatchPolic := `
+        method: GET`
+		match.SaveMatchPolicy(testMatchPolic, "servicecomb.match."+testName, testName)
+		i := createInvoker(nil, http.MethodGet, "")
+		match.Mark(i)
+		assert.Equal(t, testName, i.GetMark())
+	})
+
+	t.Run("test match apipath", func(t *testing.T) {
+		testName := "match-user-json-apipath"
+		testMatchPolic := `
+        apiPath: 
+          contains: "path/test"`
+		match.SaveMatchPolicy(testMatchPolic, "servicecomb.match."+testName, testName)
+		i := createInvoker(nil, http.MethodPost, "cse://127.0.0.1:9992/path/test")
+		match.Mark(i)
+		assert.Equal(t, testName, i.GetMark())
+	})
+}
+
+func createInvoker(headers map[string]string, method, url string) *invocation.Invocation {
+	i := invocation.New(context.Background())
+	i.Metadata = make(map[string]interface{})
+	for k, v := range headers {
+		i.SetHeader(k, v)
+	}
+	i.Args, _ = rest.NewRequest(method, url, nil)
+	return i
 }
