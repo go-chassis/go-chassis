37c346aae3ea47fa78ee69ae7d407014b1f04824
Shawn <xiaoliang.tian@gmail.com>
2020-05-22 10:10:00 +0800

    resolve UT problem (#850)

--- eventlistener/qps_event_listener_test.go
+++ eventlistener/qps_event_listener_test.go
@@ -2,19 +2,12 @@ package eventlistener_test
 
 import (
 	"github.com/go-chassis/go-archaius/event"
-	"github.com/go-chassis/go-chassis/core/config"
 	"github.com/go-chassis/go-chassis/core/lager"
 	"github.com/go-chassis/go-chassis/eventlistener"
-	"os"
 	"testing"
 )
 
 func TestQpsEvent(t *testing.T) {
-	gopath := os.Getenv("GOPATH")
-	os.Setenv("CHASSIS_HOME", gopath+"/src/github.com/go-chassis/go-chassis/examples/discovery/server/")
-
-	t.Log("Test qps_event_listener.go")
-	config.Init()
 	eventlistener.Init()
 	eventListen := &eventlistener.QPSEventListener{}
 	t.Log("sending the events for the key cse.flowcontrol.Consumer.qps.limit.Server")
