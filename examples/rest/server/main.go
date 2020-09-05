e2948ca15f8d450807ccd97e5dbf241eeb1f76b5
tian <xiaoliang.tian@gmail.com>
2020-09-05 09:54:36 +0800

    png for docs page

--- examples/rest/server/main.go
+++ examples/rest/server/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/go-chassis/go-chassis/v2"
-	"github.com/go-chassis/go-chassis/v2/core/server"
 	"github.com/go-chassis/go-chassis/v2/examples/schemas"
 	"github.com/go-chassis/openlog"
 )
@@ -10,7 +9,7 @@ import (
 //if you use go run main.go instead of binary run, plz export CHASSIS_HOME=/{path}/{to}/rest/server/
 
 func main() {
-	chassis.RegisterSchema("rest", &schemas.RestFulHello{}, server.WithSchemaID("RestHelloService"))
+	chassis.RegisterSchema("rest", &schemas.RestFulHello{})
 	if err := chassis.Init(); err != nil {
 		openlog.Fatal("Init failed." + err.Error())
 		return
