84880315a6611876ca9effa7ded5d43ba2861158
kkf1 <2644863636@qq.com>
2022-08-24 19:07:07 +0800

    add SummaryValue interface to fetch SampleSum and SampleCount values from summary metric

--- pkg/metrics/metrics_test.go
+++ pkg/metrics/metrics_test.go
@@ -117,20 +117,23 @@ func TestSummaryObserve(t *testing.T) {
 	labels := map[string]string{
 		"service": "s",
 	}
-	err = metrics.SummaryObserve("latency", 1, labels)
-	assert.NoError(t, err)
+	t.Run("testSummaryObserve", func(t *testing.T) {
+		err = metrics.SummaryObserve("latency", 1, labels)
+		assert.NoError(t, err)
 
-	count, sum := metrics.SummaryValue("latency", labels)
-	assert.Equal(t, uint64(1), count)
-	assert.Equal(t, float64(1), sum)
+		count, sum := metrics.SummaryValue("latency", labels)
+		assert.Equal(t, uint64(1), count)
+		assert.Equal(t, float64(1), sum)
 
-	count, sum = metrics.SummaryValue("latency", nil)
-	assert.Equal(t, uint64(1), count)
-	assert.Equal(t, float64(1), sum)
+		count, sum = metrics.SummaryValue("latency", nil)
+		assert.Equal(t, uint64(1), count)
+		assert.Equal(t, float64(1), sum)
 
-	count, sum = metrics.SummaryValue("latency", map[string]string{"service": "test"})
-	assert.Equal(t, uint64(0), count)
-	assert.Equal(t, float64(0), sum)
+		count, sum = metrics.SummaryValue("latency", map[string]string{"service": "test"})
+		assert.Equal(t, uint64(0), count)
+		assert.Equal(t, float64(0), sum)
+	})
+
 }
 func TestCreateHistogram(t *testing.T) {
 	err := metrics.HistogramObserve("hlatency", 1, map[string]string{
